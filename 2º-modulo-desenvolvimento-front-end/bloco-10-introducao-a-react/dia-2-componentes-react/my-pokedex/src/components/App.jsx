f434462e651b1df00c947667b89b9f80f0352c22
leandro-miranda <leandromiranda2010@gmail.com>
2022-06-21 11:36:36 -0300

    finalizado app

--- 2º-modulo-desenvolvimento-front-end/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/components/App.jsx
+++ 2º-modulo-desenvolvimento-front-end/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Pokedex from "../components/Pokedex"
+import Header from "../components/Header"
 import '../index.css';
 
 class App extends React.Component {
   render() {
     return (
       <div className='App'>
+        <Header />
         <Pokedex />
       </div>
     )
