eed22b7243095cf159b8d9bee197eb4fc84c6709
leandro-miranda <leandromiranda2010@gmail.com>
2022-06-22 20:00:49 -0300

    criando button jsx

---
+++ 2º-modulo-desenvolvimento-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/exercicios-11.1/exercise-pokedex-state/src/components/Button.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { bool, func, node, string } from 'prop-types';
+import './button.css';
+
+const Button = ({ className, children, disabled, onClick }) => (
+  <button
+    onClick={ onClick }
+    className={ `button-text ${className}` }
+    disabled={ disabled }
+    type="button"
+  >
+    {children}
+  </button>
+);
+
+Button.propTypes = {
+  className: string.isRequired,
+  children: node.isRequired,
+  disabled: bool,
+  onClick: func.isRequired,
+};
+
+Button.defaultProps = {
+  disabled: false,
+};
+
+export default Button;
