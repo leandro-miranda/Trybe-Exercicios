1919ab0fe10f84dfbc351d09cc1a8bc66e7ba37f
leandro-miranda <leandromiranda2010@gmail.com>
2022-06-23 11:42:04 -0300

    arrumando as className

--- 2º-modulo-desenvolvimento-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/exercicios-11.1/exercise-pokedex-state/src/components/Pokedex.jsx
+++ 2º-modulo-desenvolvimento-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/exercicios-11.1/exercise-pokedex-state/src/components/Pokedex.jsx
@@ -48,19 +48,19 @@ class Pokedex extends React.Component {
         {pokemons.map((pokemon) => (
           <Pokemon key={ pokemon.id } pokemon={ pokemon } />
         ))[index]}
-        <div className="pokedex=buttons-panel">
+        <div className="pokedex-buttons-panel">
           <Button
+            className="filter-button"
             name="Próximo Pokemon"
             func={ this.handleClick }
             disabled={ pokemons.length === 1 }
-            className="filter=button"
           />
           {types.map((type) => (
             <Button
+              className="filter-button"
               name={ type }
               key={ type }
               func={ () => this.handleClickFilter(type) }
-              className="filter=button"
             />
           ))}
         </div>
@@ -74,75 +74,3 @@ Pokedex.propTypes = {
 };
 
 export default Pokedex;
-
-// class Pokedex extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       pokemonIndex: 0,
-//       filteredType: 'all',
-//     };
-//   }
-
-//   filterPokemons(filteredType) {
-//     this.setState({ filteredType, pokemonIndex: 0 });
-//   }
-
-//   nextPokemon(numberOfPokemons) {
-//     this.setState((state) => ({
-//       pokemonIndex: (state.pokemonIndex + 1) % numberOfPokemons,
-//     }));
-//   }
-
-//   fetchFilteredPokemons() {
-//     const { pokemons } = this.props;
-//     const { filteredType } = this.state;
-
-//     return pokemons.filter((pokemon) => {
-//       if (filteredType === 'all') return true;
-//       return pokemon.type === filteredType;
-//     });
-//   }
-
-//   fetchPokemonTypes() {
-//     const { pokemons } = this.props;
-//     return [...new Set(pokemons.reduce((types, { type }) => [...types, type], []))];
-//   }
-
-//   render() {
-//     const filteredPokemons = this.fetchFilteredPokemons();
-//     const pokemonTypes = this.fetchPokemonTypes();
-//     const { pokemonIndex } = this.state;
-//     const pokemon = filteredPokemons[pokemonIndex];
-
-//     return (
-//       <div className="pokedex">
-//         <Pokemon pokemon={ pokemon } />
-//         <div className="pokedex=buttons-panel">
-//           <Button
-//             onClick={ () => this.filterPokemons('all') }
-//             className="filter=button"
-//           >
-//             All
-//           </Button>
-//           {pokemonTypes.map((type) => (
-//             <Button
-//               key={ type }
-//               onClick={ () => this.filterPokemons(type) }
-//               className="filter-button"
-//             >
-//               {type}
-//             </Button>
-//           ))}
-//         </div>
-//         <Button
-//           className="pokedex-button"
-//           onClick={ () => this.nextPokemon(filteredPokemons.length) }
-//           disabled={ filteredPokemons.length <= 1 }
-//         >
-//           Próximo pokémon
-//         </Button>
-//       </div>
-//     );
-//   }
-// }
