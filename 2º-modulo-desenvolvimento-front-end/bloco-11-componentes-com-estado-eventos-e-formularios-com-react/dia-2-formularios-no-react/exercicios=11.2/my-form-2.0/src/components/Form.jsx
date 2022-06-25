107e789de18d9bf1cbe1171532d3b5eeb993d1e4
leandro-miranda <leandromiranda2010@gmail.com>
2022-06-25 15:45:51 -0300

    criando o form e passando props

--- 2º-modulo-desenvolvimento-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/exercicios=11.2/my-form-2.0/src/components/Form.jsx
+++ 2º-modulo-desenvolvimento-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/exercicios=11.2/my-form-2.0/src/components/Form.jsx
@@ -1,7 +1,6 @@
-import React, { Component } from 'react'
-
-const states = ['Rio de Janeiro', 'Minas Gerais', 'Amapá', 'Amazonas',
-  'São Paulo', 'Ceará', 'Distrito Federal'];
+import React, { Component } from 'react';
+import PersonalForm from './PersonalForm';
+import ProfessionalForm from './ProfessionalForm';
 
 const INITIAL_STATE = {
   name: '',
@@ -17,10 +16,10 @@ export default class Form extends Component {
   constructor(props) {
     super(props)
     this.state = INITIAL_STATE;
-}
-
-  validateAddress = (address) => address.replace(/[^\w\s]/gi, '');
+  }
   
+  validateAddress = (address) => address.replace(/[^\w\s]/gi, '');
+    
   handleChange = (event) => {
     const { name } = event.target;
     let { value } = event.target;
@@ -40,104 +39,20 @@ export default class Form extends Component {
       [name]: value,
     });
   }
-  
+
   render() {
-    const { name, address } = this.state;
-     return (
-      <fieldset>
-        <legend>Dados Pessoais</legend>
-        <div className="container">
-          Nome: 
-          <input
-            type='name'
-            name='name'
-            maxLength='40'
-            value={name}
-            required
-            onChange={this.handleChange}
-          />
-        </div>
-        <div className="container">
-          Email:
-          <input
-            type='email'
-            name='email'
-            maxLength='50'
-            required
-            onChange={this.handleChange}
-          />
-        </div>
-        <div className="container">
-          CPF:
-          <input
-            type='text'
-            name='cpf'
-            maxLength='11'
-            required
-            onChange={this.handleChange}
-          />
-        </div>
-        <div className="container">
-          Endereço:
-          <input
-            type='text'
-            name='address'
-            maxLength='200'
-             required
-             value={address}
-            onChange={this.handleChange}
-          />
-        </div>
-        <div className="container">
-          Cidade:
-          <input 
-            type='text'
-            name='city'
-            maxLength='28'
-            required
-            onBlur={this.handleBlur}
-            onChange={this.handleChange}
-          />
-        </div>
-        <div className="container">
-          Estado:
-          <select
-            name='countryState'
-            required
-            onChange={this.handleChange}
-            defaultValue=''
-          >
-            <option value="Selecione"></option>
-            {
-              states.map((value, key) => (
-                <option key={key}>{value }</option>
-              ))
-            }
-          </select>
-        </div>
-        <div className="container">
-          <label htmlFor='house'>
-            <input
-              type='radio'
-              id='house'
-              name='addressType'
-              value='house'
-              onChange={this.handleChange}
-            />
-            Casa
-          </label>
-          <label htmlFor="apart">
-            <input
-              type='radio'
-              id='apart'
-              name='addressType'
-              value='apart'
-              onChange={this.handleChange}
-            />
-            Apartamento
-          </label>
-        </div>
-      </fieldset>
-    )
+    return (
+      <form>
+        <h1>Cadastro</h1>
+        <PersonalForm
+          onHandleChange={this.handleChange}
+          onHandleBlur={this.handleBlur}
+        />
+        <ProfessionalForm
+          onHandleChange={this.handleChange}
+          onHandleBlur={this.handleBlur}
+        />
+      </form>
+    );
   }
 }
