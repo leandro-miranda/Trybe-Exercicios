0aa98149b71d0d5080a89f5eba646bc0c8371130
leandro-miranda <leandromiranda2010@gmail.com>
2022-06-25 16:26:55 -0300

    criando form data display

---
+++ 2º-modulo-desenvolvimento-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/exercicios=11.2/my-form-2.0/src/components/FormDataDisplay.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+export default class FormDataDisplay extends Component {
+  render() {
+    const {
+      state: {
+        name, email, cpf, address, city, countryState,
+        addressType, resume, role, roleDescription,
+      },
+    } = this.props;
+    return (
+      <div>
+        <h2>Dados enviados</h2>
+        <h3>Pessoal</h3>
+        <div>
+          <p>{`Name: ${name}`}</p>
+          <p>{`Email: ${email}`}</p>
+          <p>{`CPF: ${cpf}`}</p>
+          <p>{`Endereço: ${address}`}</p>
+          <p>{`Cidade: ${city} - ${countryState}`}</p>
+          <p>{`Tipo de Residência: ${addressType}`}</p>
+        </div>
+
+        <h3>Profissional</h3>
+        <div>
+          <p>{`Currículo: ${resume}`}</p>
+          <p>{`Cargo: ${role}`}</p>
+          <p>{`Descrição do Cargo: ${roleDescription}`}</p>
+          
+        </div>
+      </div>
+    )
+  }
+}
