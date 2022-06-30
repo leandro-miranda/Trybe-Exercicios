119bc6396576833b26018a734db36e4d9a73e600
leandro-miranda <leandromiranda2010@gmail.com>
2022-06-30 18:59:14 -0300

    criando button

---
+++ 2º-modulo-desenvolvimento-front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-2-react-router/exercise-pokedex-router/src/components/Button.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import '../styles/button.css';
+
+export default class Button extends Component {
+  render() {
+    const { className, children, disabled, onClick } = this.props;
+    return (
+      <button
+        onClick={ onClick }
+        className={ `button-text ${className}` }
+        disabled={ disabled }
+        type="button"
+      >
+        {children}
+      </button>
+    );
+  }
+}
+
+Button.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+  disabled: PropTypes.bool,
+  onClick: PropTypes.func.isRequired,
+};
+
+Button.defaultProps = {
+  children: null,
+  className: '',
+  disabled: false,
+};
