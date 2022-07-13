af3387bf312275989d687e8824f4d92d2f4bec4a
leandro-miranda <leandromiranda2010@gmail.com>
2022-07-13 19:20:14 -0300

    um outro jeito de verificar se o botão está na tela com o texto Adicionar

--- 2º-modulo-desenvolvimento-front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-1-rtl-primeiros-passos/exercise-todo-list/src/test/exerciseOne.test.js
+++ 2º-modulo-desenvolvimento-front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-1-rtl-primeiros-passos/exercise-todo-list/src/test/exerciseOne.test.js
@@ -9,9 +9,13 @@ describe('Testando a aplicação, testando botão, e sua funcionalidade', () => 
   test('Verifica se o botão está na tela com o texto "Adicionar"', () => {
     render(<App />);
 
-    const buttonAdd = screen.getByText('Adicionar');
-    expect(buttonAdd).toBeInTheDocument();
-    expect(buttonAdd.type).toBe('button');
+    // const buttonAdd = screen.getByText('Adicionar');
+    // expect(buttonAdd).toBeInTheDocument();
+    // expect(buttonAdd.type).toBe('button');
+    // ou
+
+    const button = screen.getByRole('button', { name: 'Adicionar' });
+    expect(button).toBeInTheDocument();
   });
 
   test('Ao clicar no botão "Adicionar" a task deve ser adicionada a tela', () => {
