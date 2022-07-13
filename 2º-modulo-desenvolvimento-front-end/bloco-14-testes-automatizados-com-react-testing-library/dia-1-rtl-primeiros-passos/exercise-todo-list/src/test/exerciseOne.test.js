05727747fc31349816e863fddff9b0df4cd2b850
leandro-miranda <leandromiranda2010@gmail.com>
2022-07-13 19:02:35 -0300

    teste se ao clicar no botão a task é adicionada

--- 2º-modulo-desenvolvimento-front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-1-rtl-primeiros-passos/exercise-todo-list/src/test/exerciseOne.test.js
+++ 2º-modulo-desenvolvimento-front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-1-rtl-primeiros-passos/exercise-todo-list/src/test/exerciseOne.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 
 const TASK_DESCRIPTION = 'Beber água!';
@@ -18,6 +19,9 @@ describe('Testando a aplicação, testando botão, e sua funcionalidade', () => 
 
     const inputTask = screen.getByLabelText('Tarefa:');
     const buttonAdd = screen.queryByText('Adicionar');
-    
+    userEvent.type(inputTask, TASK_DESCRIPTION);
+    expect(screen.queryByText(TASK_DESCRIPTION)).not.toBeInTheDocument();
+    userEvent.click(buttonAdd);
+    expect(screen.queryByText(TASK_DESCRIPTION)).toBeInTheDocument();
   });
 });
