4c07e4f9c57118a75b40d90ab13c33711f6cbf24
leandro-miranda <leandromiranda2010@gmail.com>
2022-07-18 13:55:47 -0300

    arrumando test jest

---
+++ 2º-modulo-desenvolvimento-front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/exercicies/service.js
@@ -0,0 +1,24 @@
+const randomNumber = () => Math.floor(Math.random() * 101);
+
+module.exports = { randomNumber };
+
+const firstFunction = (str) => str.toUpperCase();
+const secondFunction = (str) =>  str.charAt(0);
+const thirdFunction = (str1, str2) => str1.concat(str2);
+
+module.exports = {
+  firstFunction,
+  secondFunction,
+  thirdFunction,
+};
+
+function fetchDog() {
+  return fetch("https://dog.ceo/api/breeds/image/random").then(response =>
+    response
+      .json()
+      .then(json =>
+        response.ok ? Promise.resolve(json) : Promise.reject(json)
+      )
+  );
+}
+module.exports = { fetchDog };
