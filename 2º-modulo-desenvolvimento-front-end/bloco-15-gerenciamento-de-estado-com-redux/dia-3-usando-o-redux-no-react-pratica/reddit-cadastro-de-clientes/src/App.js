5b8e85eefd63b30a74326f68fc85797e6bc63df4
leandro-miranda <leandromiranda2010@gmail.com>
2022-07-26 14:25:32 -0300

    provider store

--- 2º-modulo-desenvolvimento-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-3-usando-o-redux-no-react-pratica/reddit-cadastro-de-clientes/src/App.js
+++ 2º-modulo-desenvolvimento-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-3-usando-o-redux-no-react-pratica/reddit-cadastro-de-clientes/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {Provider} from 'react-redux';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import store from './redux/store';
+import Home from '.pages/Home';
+import Login from '.pages/Login';
+import Register from '.pages/Register';
+import CostumersList from '.pages/CostumersList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact patch='/' component={Home} />
+            <Route patch='/login' component={Login} />
+            <Route patch='/register' component={Register} />
+            <Route patch='/costumers' component={CostumersList} />
+          </Switch>
+        </BrowserRouter>
+      </Provider>
+    )
+  }
 }
 
-export default App;
