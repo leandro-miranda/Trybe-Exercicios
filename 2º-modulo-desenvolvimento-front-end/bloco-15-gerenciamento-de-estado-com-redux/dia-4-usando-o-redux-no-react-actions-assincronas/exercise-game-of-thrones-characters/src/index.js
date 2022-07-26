d8ec0fa902799322c5ec90a274870e9250d4831d
leandro-miranda <leandromiranda2010@gmail.com>
2022-07-26 20:13:02 -0300

    arrumando import store

--- 2º-modulo-desenvolvimento-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-4-usando-o-redux-no-react-actions-assincronas/exercise-game-of-thrones-characters/src/index.js
+++ 2º-modulo-desenvolvimento-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-4-usando-o-redux-no-react-actions-assincronas/exercise-game-of-thrones-characters/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 
-import store from './store';
+import store from '../src/redux/store';
 
 ReactDOM.render(
   <Provider store={store}>
