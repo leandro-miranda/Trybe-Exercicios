72596dd73e8042bc664a30b947fdea7460fae65d
leandro-miranda <leandromiranda2010@gmail.com>
2022-07-27 10:42:29 -0300

    import css

--- 2º-modulo-desenvolvimento-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-3-usando-o-redux-no-react-pratica/reddit-cadastro-de-clientes/src/App.js
+++ 2º-modulo-desenvolvimento-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-3-usando-o-redux-no-react-pratica/reddit-cadastro-de-clientes/src/App.js
@@ -6,6 +6,7 @@ import Home from '.pages/Home';
 import Login from '.pages/Login';
 import Register from '.pages/Register';
 import CostumersList from '.pages/CostumersList';
+import './App.css';
 
 export default class App extends Component {
   render() {
