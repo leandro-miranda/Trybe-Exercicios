498898a22d023f83cee4214de89d4a8655e14217
leandro-miranda <leandromiranda2010@gmail.com>
2022-08-12 19:37:17 -0300

    refatorando para context API

---
+++ 2º-modulo-desenvolvimento-front-end/bloco-17-context-api-e-react-hooks/dia-1-context-api-do-react/exercise-contextAPI-refactoring/src/TrafficSignal.jsx
@@ -0,0 +1,39 @@
+// src/TrafficSignal.jsx
+import React, {Component} from 'react';
+import CarsContext from './context/CarsContext';
+import redSignal from './images/redSignal.jpeg';
+import yellowSignal from './images/yellowSignal.jpeg';
+import greenSignal from './images/greenSignal.jpeg';
+
+const renderSignal = (signalColor) => {
+  if (signalColor === 'red') return redSignal;
+  if (signalColor === 'yellow') return yellowSignal;
+  if (signalColor === 'green') return greenSignal;
+  return null;
+};
+
+export default class TrafficSignal extends Component {
+  render() {
+    const {colors} = this.context.signal;
+    const {changeSignal} = this.context;
+
+  return (
+      <div>
+        <div className="button-container">
+          <button onClick={() => changeSignal('red')} type="button">
+            Red
+          </button>
+          <button onClick={() => changeSignal('yellow')} type="button">
+            Yellow
+          </button>
+          <button onClick={() => changeSignal('green')} type="button">
+            Green
+          </button>
+        </div>
+        <img className="signal" src={renderSignal(colors)} alt="" />
+      </div>
+    );
+  }
+};
+
+TrafficSignal.contextType = CarsContext;
