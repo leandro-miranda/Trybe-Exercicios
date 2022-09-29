59011fe5073c56339d2c7ecff898b123f958f1d7
leandro-miranda <leandromiranda2010@gmail.com>
2022-09-29 20:53:47 -0300

    criando testes

--- 3º-modulo-desenvolvimento-back-end/bloco-22-indroducao-ao-desenvolvimento-web-com-nodejs/dia-3-nodejs-testes-de-integracao/cacao-trybe/tests/integration/chocolates.test.js
+++ 3º-modulo-desenvolvimento-back-end/bloco-22-indroducao-ao-desenvolvimento-web-com-nodejs/dia-3-nodejs-testes-de-integracao/cacao-trybe/tests/integration/chocolates.test.js
@@ -1,26 +1,118 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const sinon = require('sinon');
+const fs = require('fs');
 const app = require('../../src/app');
 
 const {expect} = chai;
 
 chai.use(chaiHttp);
 
-describe('Usando o método GET em /chocolates', function () {
-  it('Retorna a lista completa de chocolates', async function () {
-    const output = [
-      { id: 1, name: 'Mint Intense', brandId: 1 },
-      { id: 2, name: 'White Coconut', brandId: 1 },
-      { id: 3, name: 'Mon Chéri', brandId: 2 },
-      { id: 4, name: 'Mounds', brandId: 3 },
-    ];
+const mockFile = JSON.stringify({ 
+  brands: [
+    {
+      id: 1,
+      name: 'Lindt & Sprungli',
+    },
+    {
+      id: 2,
+      name: 'Ferrero',
+    },
+    {
+      id: 3,
+      name: 'Ghirardelli',
+    },
+  ],
+  chocolates: [
+    {
+      id: 1,
+      name: 'Mint Intense',
+      brandId: 1,
+    },
+    {
+      id: 2,
+      name: 'White Coconut',
+      brandId: 1,
+    },
+    {
+      id: 3,
+      name: 'Mon Chéri',
+      brandId: 2,
+    },
+    {
+      id: 4,
+      name: 'Mounds',
+      brandId: 3,
+    },
+  ],
+});
 
-    const response = await chai
-      .request(app)
-      .get('/chocolates');
+// O stub irá interceptar, durante o teste, todas as chamadas a função readFile(), portanto, é imprescindível que os valores passados em resolves sejam do mesmo tipo que a função original.
+describe('Testando a API Cacao Trybe', function () {
+  beforeEach(function () {
+    sinon.stub(fs.promises, 'readFile').resolves(mockFile);
+  });
 
-    expect(response.status).to.be.equal(200);
-    expect(response.body.chocolates).to.deep.equal(output);
-    // utilizamos o deep para garantir que todas as informações dentro do objeto retornado são as mesmas do objeto esperado. Do outro modo, essa validação não seria possível, pois não seria realizada a comparação em profundidade.
-  })
-})
+  afterEach(function () {
+    sinon.restore();
+  });
+
+  describe('Usando o método GET em /chocolates', function () {
+    it('Retorna a lista completa de chocolates', async function () {
+      const output = [
+        { id: 1, name: 'Mint Intense', brandId: 1 },
+        { id: 2, name: 'White Coconut', brandId: 1 },
+        { id: 3, name: 'Mon Chéri', brandId: 2 },
+        { id: 4, name: 'Mounds', brandId: 3 },
+      ];
+  
+      const response = await chai
+        .request(app)
+        .get('/chocolates');
+  
+      expect(response.status).to.be.equal(200);
+      expect(response.body.chocolates).to.deep.equal(output);
+      // utilizamos o deep para garantir que todas as informações dentro do objeto retornado são as mesmas do objeto esperado. Do outro modo, essa validação não seria possível, pois não seria realizada a comparação em profundidade.
+    });
+  });
+
+  describe('Usando o método GET em /chocolates/:id para buscar o ID 4', function () {
+    it('Retorna o chocolate Mounds', async function () {
+      const response = await chai
+        .request(app)
+        .get('/chocolates/4');
+
+      expect(response.status).to.be.equal(200);
+      expect(response.body.chocolates).to.deep.equal([
+        {
+          id: 4,
+          name: 'Mounds',
+          brandId: 3,
+        }]);
+    });
+  });
+
+  describe('Usando o método GET em /chocolates/brand/:brandId para buscar brandId 1', function () {
+    it('Retorna os chocolates da marca Lindt & Sprungli', async function () {
+      const response = await chai
+        .request(app)
+        .get('chocolates/brand/1');
+
+      expect(response.status).to.be.equal(200);
+      expect(response.body.chocolates).to.deep.equal([
+        {
+          id: 1,
+          name: 'Mint Intense',
+          brandId: 1,
+        },
+        {
+          id: 2,
+          name: 'White Coconut',
+          brandId: 1,
+        },
+      ]);
+    });
+  });
+});
+
+//  A função restore() desempenha um papel muito importante quando utilizamos stubs, pois é ela que vai garantir que o stub de um teste não seja replicado para os testes seguintes.
