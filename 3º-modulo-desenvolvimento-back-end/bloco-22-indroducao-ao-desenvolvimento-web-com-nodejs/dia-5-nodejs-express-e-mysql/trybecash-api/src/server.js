71cb716a43eb61449388303610e86b6214d5b4a8
leandro-miranda <leandromiranda2010@gmail.com>
2022-10-03 14:09:34 -0300

    criando arquivo server.js

---
+++ 3º-modulo-desenvolvimento-back-end/bloco-22-indroducao-ao-desenvolvimento-web-com-nodejs/dia-5-nodejs-express-e-mysql/trybecash-api/src/server.js
@@ -0,0 +1,20 @@
+const app = require('./app');
+// const connection = require('./db/connection');
+
+const port = 3001;
+
+app.listen(port, async () => {
+  console.log(`API TrybeCash está sendo executada na porta ${port}`);
+
+  // o código abaixo é para testarmos a comunicação com o MYSQL
+  // const [ result ] = await connection.execute('SELECT 1');
+  // if (result) {
+  //   console.log('MySQL connection OK');
+  // }
+});
+
+// No arquivo src/server.js criamos uma constante app que realiza um require do arquivo src/app.js (lembra do module.exports que declaramos?) e, a partir dessa constante, iniciamos o nosso express executando a função app.listen().
+
+// Dentro da função app.listen() foi adicionado um trecho de código que executa a função connection.execute(), que recebe como parâmetro uma consulta SQL SELECT 1. Essa função realiza uma conexão com o MySQL, executa o SQL passado como parâmetro e recebe uma resposta que é armazenada na constante result (note que o processo de desestruturação de variáveis está sendo utilizado! 😎).
+
+// Depois é avaliado através de um if se o objeto result contém alguma coisa e, em caso de positivo, é impresso no console a mensagem MySQL connection OK. Se você for no console e executar o comando npm start, o express será iniciado.
