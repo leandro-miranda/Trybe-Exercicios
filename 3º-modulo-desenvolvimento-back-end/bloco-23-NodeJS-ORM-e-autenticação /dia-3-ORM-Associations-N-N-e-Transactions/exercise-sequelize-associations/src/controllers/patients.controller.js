4636c25ca41ec9ad0e978d39af8db86f270289af
leandro-miranda <leandromiranda2010@gmail.com>
2022-10-21 20:20:12 -0300

    criando endpoint que liste todos os pacientes e suas respectivas cirurgias realizadas

--- 3º-modulo-desenvolvimento-back-end/bloco-23-NodeJS-ORM-e-autenticação /dia-3-ORM-Associations-N-N-e-Transactions/exercise-sequelize-associations/src/controllers/patients.controller.js
+++ 3º-modulo-desenvolvimento-back-end/bloco-23-NodeJS-ORM-e-autenticação /dia-3-ORM-Associations-N-N-e-Transactions/exercise-sequelize-associations/src/controllers/patients.controller.js
@@ -1,15 +1,28 @@
-const { Patient, Plan } = require('../models');
+const { Patient, Plan, Surgery } = require('../models');
 
 const getAllPatientsPlans = async (req, res) =>
   Patient.findAll({ include: { model: Plan, as: 'plan' } })
-    .then((listOfPatients) => {
-      if (!listOfPatients.length) {
+    .then((listOfPatientsPlans) => {
+      if (!listOfPatientsPlans.length) {
         return res.status(404).send({ message: 'No patients found' });
       }
-      return res.status(200).json(listOfPatients);
+      return res.status(200).json(listOfPatientsPlans);
     })
     .catch(() => res.status(500).json({ message: 'Algo deu errado!' }));
 
+const getAllPatientsSurgeries = (_req, res) =>
+  Patient.findAll({
+    include: { model: Surgery, as: 'surgeries', through: { attributes: [] } },
+  })
+    .then((listOfPatientsSugeries) => {
+      if (!listOfPatientsSugeries.length) {
+        return res.status(404).send({ message: 'No patients found' });
+      }
+      return res.status(200).json(listOfPatientsSugeries);
+    })
+    .catch(() => () => res.status(500).json({ message: 'Algo deu errado' }));
+
 module.exports = {
   getAllPatientsPlans,
+  getAllPatientsSurgeries,
 };
