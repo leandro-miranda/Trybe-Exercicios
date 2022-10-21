fb992fd89c4cb8acb22aa339d67260cab312b845
leandro-miranda <leandromiranda2010@gmail.com>
2022-10-21 20:36:34 -0300

    criando get :id

--- 3º-modulo-desenvolvimento-back-end/bloco-23-NodeJS-ORM-e-autenticação /dia-3-ORM-Associations-N-N-e-Transactions/exercise-sequelize-associations/src/app.js
+++ 3º-modulo-desenvolvimento-back-end/bloco-23-NodeJS-ORM-e-autenticação /dia-3-ORM-Associations-N-N-e-Transactions/exercise-sequelize-associations/src/app.js
@@ -4,10 +4,13 @@ const {
   getAllPatientsSurgeries,
 } = require('./controllers/patients.controller');
 
+const { getAllPlans } = require('./controllers/plans.controller');
+
 const app = express();
 app.use(express.json());
 
 app.get('/patients', getAllPatientsPlans);
 app.get('/surgeries', getAllPatientsSurgeries);
+app.get('/:id', getAllPlans);
 
 module.exports = app;
