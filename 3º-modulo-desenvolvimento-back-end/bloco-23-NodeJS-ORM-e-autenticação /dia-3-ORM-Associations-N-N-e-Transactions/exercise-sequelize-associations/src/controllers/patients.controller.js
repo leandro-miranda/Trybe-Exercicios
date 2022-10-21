0ba447b8c19f21510c79d0c527943916b6d3d817
leandro-miranda <leandromiranda2010@gmail.com>
2022-10-21 20:40:02 -0300

    criando endpoint que liste todos os pacientes e suas cirurgias realizadas, mas oculte o nome do médico responsável.

--- 3º-modulo-desenvolvimento-back-end/bloco-23-NodeJS-ORM-e-autenticação /dia-3-ORM-Associations-N-N-e-Transactions/exercise-sequelize-associations/src/controllers/patients.controller.js
+++ 3º-modulo-desenvolvimento-back-end/bloco-23-NodeJS-ORM-e-autenticação /dia-3-ORM-Associations-N-N-e-Transactions/exercise-sequelize-associations/src/controllers/patients.controller.js
@@ -22,7 +22,35 @@ const getAllPatientsSurgeries = (_req, res) =>
     })
     .catch(() => () => res.status(500).json({ message: 'Algo deu errado' }));
 
+const createPatient = (req, res) => {
+  const { fullname, planId } = req.body;
+  Patient.create({ fullname, planId })
+    .then((response) => res.status(200).json(response))
+    .catch(() => res.status(500).json({ message: 'Algo deu errado' }));
+};
+
+const getPatientsAndSurgeriesNoDoctor = (_req, res) => (
+  Patient.findAll({
+    include: {
+      model: Surgery,
+      as: 'surgeries',
+      attributes: { exclude: ['doctor'] },
+      through: { attributes: [] },
+    },
+  })
+    .then((listOfPatients) => {
+      if (listOfPatients === null) {
+        return res.status(404).send({ message: 'No patients found' });
+      }
+
+      return res.status(200).json(listOfPatients);
+    })
+    .catch(() => res.status(500).json({ message: 'Algo deu errado' }))
+);
+
 module.exports = {
   getAllPatientsPlans,
   getAllPatientsSurgeries,
+  createPatient,
+  getPatientsAndSurgeriesNoDoctor,
 };
