54e5eb2530e39566bb466a73da7e4fab49b281c3
leandro-miranda <leandromiranda2010@gmail.com>
2022-10-21 20:08:01 -0300

    criando rota patients

--- 3º-modulo-desenvolvimento-back-end/bloco-23-NodeJS-ORM-e-autenticação /dia-3-ORM-Associations-N-N-e-Transactions/exercise-sequelize-associations/src/app.js
+++ 3º-modulo-desenvolvimento-back-end/bloco-23-NodeJS-ORM-e-autenticação /dia-3-ORM-Associations-N-N-e-Transactions/exercise-sequelize-associations/src/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const { getAllPatientsPlans } = require('./controllers/patients.controller');
 
 const app = express();
 app.use(express.json());
+
+app.get('/patients', getAllPatientsPlans);
 
 module.exports = app;
