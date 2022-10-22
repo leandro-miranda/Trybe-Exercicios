9239ca84186742514e1c24bada57df06869ad4dd
leandro-miranda <leandromiranda2010@gmail.com>
2022-10-22 10:55:16 -0300

    refatorando

--- 3º-modulo-desenvolvimento-back-end/bloco-23-NodeJS-ORM-e-autenticação /dia-3-ORM-Associations-N-N-e-Transactions/exercise-sequelize-associations/src/controllers/patients.controller.js
+++ 3º-modulo-desenvolvimento-back-end/bloco-23-NodeJS-ORM-e-autenticação /dia-3-ORM-Associations-N-N-e-Transactions/exercise-sequelize-associations/src/controllers/patients.controller.js
@@ -13,43 +13,54 @@ const getAllPatientsPlans = async (_req, res) => {
   }
 };
 
-const getAllPatientsSurgeries = (_req, res) =>
-  Patient.findAll({
-    include: { model: Surgery, as: 'surgeries', through: { attributes: [] } },
-  })
-    .then((listOfPatientsSugeries) => {
-      if (!listOfPatientsSugeries.length) {
-        return res.status(404).send({ message: 'No patients found' });
-      }
-      return res.status(200).json(listOfPatientsSugeries);
-    })
-    .catch(() => () => res.status(500).json({ message: 'Algo deu errado' }));
+const getAllPatientsSurgeries = async (_req, res) => {
+  try {
+    const listOfPatientsSugeries = await Patient.findAll({
+      include: { model: Surgery, as: 'surgeries', through: { attributes: [] } },
+    });
 
-const createPatient = (req, res) => {
-  const { fullname, planId } = req.body;
-  Patient.create({ fullname, planId })
-    .then((response) => res.status(200).json(response))
-    .catch(() => res.status(500).json({ message: 'Algo deu errado' }));
+    if (!listOfPatientsSugeries.length) {
+      return res.status(404).send({ message: 'No patients found' });
+    }
+    return res.status(200).json(listOfPatientsSugeries);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Algo deu errado' });
+  }
 };
 
-const getPatientsAndSurgeriesNoDoctor = (_req, res) => (
-  Patient.findAll({
-    include: {
-      model: Surgery,
-      as: 'surgeries',
-      attributes: { exclude: ['doctor'] },
-      through: { attributes: [] },
-    },
-  })
-    .then((listOfPatients) => {
-      if (listOfPatients === null) {
-        return res.status(404).send({ message: 'No patients found' });
-      }
+const createPatient = async (req, res) => {
+  try {
+    const { fullname, planId } = req.body;
+    const patient = await Patient.create({ fullname, planId });
 
-      return res.status(200).json(listOfPatients);
-    })
-    .catch(() => res.status(500).json({ message: 'Algo deu errado' }))
-);
+    return res.status(201).json(patient);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Algo deu errado' });
+  }
+};
+
+const getPatientsAndSurgeriesNoDoctor = async (_req, res) => {
+  try {
+    const listOfPatients = await Patient.findAll({
+      include: {
+        model: Surgery,
+        as: 'surgeries',
+        attributes: { exclude: ['doctor'] },
+        through: { attributes: [] },
+      },
+    });
+
+    if (listOfPatients === null) {
+      return res.status(404).send({ message: 'No patients found' });
+    }
+    return res.status(200).json(listOfPatients);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Algo deu errado' });
+  }
+};
 
 module.exports = {
   getAllPatientsPlans,
