22e55f9a14d2e259df0a4e84f4276f21166b0ffc
leandro-miranda <leandromiranda2010@gmail.com>
2022-10-22 10:36:00 -0300

    refatorando

--- 3º-modulo-desenvolvimento-back-end/bloco-23-NodeJS-ORM-e-autenticação /dia-3-ORM-Associations-N-N-e-Transactions/exercise-sequelize-associations/src/controllers/patients.controller.js
+++ 3º-modulo-desenvolvimento-back-end/bloco-23-NodeJS-ORM-e-autenticação /dia-3-ORM-Associations-N-N-e-Transactions/exercise-sequelize-associations/src/controllers/patients.controller.js
@@ -1,14 +1,17 @@
 const { Patient, Plan, Surgery } = require('../models');
 
-const getAllPatientsPlans = async (req, res) =>
-  Patient.findAll({ include: { model: Plan, as: 'plan' } })
-    .then((listOfPatientsPlans) => {
-      if (!listOfPatientsPlans.length) {
-        return res.status(404).send({ message: 'No patients found' });
-      }
-      return res.status(200).json(listOfPatientsPlans);
-    })
-    .catch(() => res.status(500).json({ message: 'Algo deu errado!' }));
+const getAllPatientsPlans = async (_req, res) => {
+  try {
+    const listOfPatientsPlans = await Patient.findAll({ include: { model: Plan, as: 'plan' } });
+    if (!listOfPatientsPlans.length) {
+      return res.status(404).send({ message: 'No patients found' });
+    }
+    return res.status(200).json(listOfPatientsPlans);
+  } catch (error) {
+    console.log((error));
+    res.status(500).json({ message: 'Algo deu errado!' });
+  }
+};
 
 const getAllPatientsSurgeries = (_req, res) =>
   Patient.findAll({
