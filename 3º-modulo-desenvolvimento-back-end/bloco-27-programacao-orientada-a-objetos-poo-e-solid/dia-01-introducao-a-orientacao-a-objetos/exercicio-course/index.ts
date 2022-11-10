7df359e7e9e90f86957885e7f324e04750d23f4f
leandro-miranda <leandromiranda2010@gmail.com>
2022-11-10 11:30:38 -0300

    chamando as funcoes

---
+++ 3º-modulo-desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-poo-e-solid/dia-01-introducao-a-orientacao-a-objetos/exercicio-course/index.ts
@@ -0,0 +1,16 @@
+import Client from "./Client";
+import ItemDoPedido from "./ItemDoPedido";
+import Pedido from "./Pedido";
+
+const cliente = new Client("João");
+
+const sanduiche = new ItemDoPedido("Sanduíche", 10);
+const refrigerante = new ItemDoPedido("Refrigerante", 5);
+const batataFrita = new ItemDoPedido("Batata Frita", 5);
+
+const pedido = new Pedido(cliente, [sanduiche, refrigerante, batataFrita], "dinheiro", 0.10);
+
+console.log(pedido);
+console.log('Valor sem desconto: ', pedido.calculaTotal());
+console.log('Valor com desconto: ', pedido.calculaTotalComDesconto());
+
