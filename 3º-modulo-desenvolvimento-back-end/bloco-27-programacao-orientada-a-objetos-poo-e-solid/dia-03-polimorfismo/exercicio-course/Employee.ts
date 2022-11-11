5d7b566f6d58b7677004f2d07554d8a58f2130e0
leandro-miranda <leandromiranda2010@gmail.com>
2022-11-11 18:54:35 -0300

    copiando do exercicio anterior

---
+++ 3º-modulo-desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-poo-e-solid/dia-03-polimorfismo/exercicio-course/Employee.ts
@@ -0,0 +1,16 @@
+// 🚀 Exercício 3: Crie uma interface que representará uma pessoa funcionária.
+// `Interface`: Employee
+// `Attributes`:
+//     - registration: número do registro
+//     - salary: valor do salário
+//     - admissionDate: data de admissão
+// `Methods`:
+//     - generateRegistration: retorna uma string única gerada como registro
+
+export default interface Employee {
+  registration: string;
+  salary: number;
+  admissionDate: Date;
+
+  generateRegistration(): string;
+}
