0aaf63c065286a23dd4f67c8a41621f4dadf2fd7
leandro-miranda <leandromiranda2010@gmail.com>
2022-11-12 08:38:10 -0300

    refatorando teacher do exercicio anterior conforme novo exercicio

--- 3º-modulo-desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-poo-e-solid/dia-03-polimorfismo/exercicio-course/Teacher.ts
+++ 3º-modulo-desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-poo-e-solid/dia-03-polimorfismo/exercicio-course/Teacher.ts
@@ -1,72 +1,34 @@
-// 🚀 Exercício 5: Crie uma classe cujos objetos representem uma pessoa professora.
 // `Class`: Teacher
-// `Extends`: Person
-// `Implements`: Employee
+// `Extends`: Employee
 // `Attributes`:
 //     - subject: a disciplina lecionada pela pessoa professora
 // `Methods`:
 //     - Getters/Setters
-//     - constructor: deve receber como parâmetro nome, data de nascimento, salário
-//       e a disciplina
+//     - constructor: deve receber como parâmetro nome, data de nascimento, salário e a disciplina
 // `Validations`:
-//     - O registro deve possuir no mínimo 16 caracteres
+//     - A matrícula deve possuir no mínimo 16 caracteres
 //     - O salário não pode ser negativo.
 //     - A data de admissão não pode ser no futuro
 
 import Subject from "./Subject";
 import Employee from "./Employee";
-import Person from "./Person";
 
-export default class Teacher extends Person implements Employee {
+export default class Teacher extends Employee {
   private _subject: Subject;
-  private _registration = String();
-  private _admissionDate: Date;
 
-  constructor(name: string, birthDate: Date, private _salary: number, subject: Subject) {
-    super(name, birthDate);
+  constructor(name: string, birthDate: Date, salary: number, subject: Subject) {
+    super(name, birthDate, salary);
+
     this._subject = subject;
-    this.salary = _salary;
-    this._admissionDate = new Date();
-    this.registration = this.generateRegistration();
+    this.enrollment = this.generateRegistration();
   }
 
   get subject(): Subject {
     return this._subject;
   }
 
-  set subject(subject: Subject) {
-    this._subject = subject;
-  }
-
-  get registration(): string {
-    return this._registration;
-  }
-
-  set registration(value: string) {
-    if (value.length < 16) throw new Error("Registration must have at least 16 characters");
-
-    this._registration = value;
-  }
-
-  get salary(): number {
-    return this._salary;
-  }
-
-  set salary(value: number) {
-    if (value < 0) throw new Error("Salary cannot be negative");
-
-
-    this._salary = value;
-  }
-
-  get admissionDate(): Date {
-    return this._admissionDate;
-  }
-
-  set admissionDate(value: Date) {
-    if (value.getTime() > new Date().getTime()) throw new Error("Admission date cannot be in the future");
-
-    this._admissionDate = value;
+  set subject(value: Subject) {
+    this._subject = value;
   }
 
   generateRegistration(): string {
