b6aeba9a5b84b98ea9326ed9e59f3efe8f2648d0
leandro-miranda <leandromiranda2010@gmail.com>
2023-02-14 12:51:13 -0300

    declarando funções que calculam a área de algumas figuras geométricas e quantas pessoas estão presentes no show

--- 4°-modulo-ciencia-da-computacao/dia-01-aprendendo-python/exemplos-course/area.py
+++ 4°-modulo-ciencia-da-computacao/dia-01-aprendendo-python/exemplos-course/area.py
@@ -22,6 +22,17 @@ def circle(radius):
     return PI * radius * radius
 
 
-print("Área do quadrado:", square(10))
-print("Área do retângulo:", rectangle(2, 2))
-print("Área do círculo:", circle(3))
+if __name__ == "__main__":
+    print("Área do quadrado:", square(10))
+    print("Área do retângulo:", rectangle(2, 2))
+    print("Área do círculo:", circle(3))
+
+# Os nossos valores de teste estão sendo exibidos quando importamos o módulo.
+# Mas não queremos que isso aconteça.
+
+# Para corrigir, podemos acrescentar uma condicional ao módulo para somente
+# exibir esses valores de teste quando o módulo for executado como script.
+
+# A variável __name__ é utilizada pelo interpretador Python para identificar o
+# arquivo que esta sendo executado e seu valor será "__main__" quando invocamos
+# um módulo como script.
