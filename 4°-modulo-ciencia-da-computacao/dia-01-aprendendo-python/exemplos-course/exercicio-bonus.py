527edf9f375454bde6243d67a6a085d2ee43a051
leandro-miranda <leandromiranda2010@gmail.com>
2023-02-14 13:44:32 -0300

    exercicio bonus 3 ok

--- 4°-modulo-ciencia-da-computacao/dia-01-aprendendo-python/exemplos-course/exercicio-bonus.py
+++ 4°-modulo-ciencia-da-computacao/dia-01-aprendendo-python/exemplos-course/exercicio-bonus.py
@@ -44,9 +44,27 @@ def triangulo(n):
 
 triangulo(10)
 
+
 # Exercício 3: Crie uma função que receba um número inteiro N e retorne o
 # somatório de todos os números de 1 até N. Por exemplo,
 # para N = 5, o valor esperado será 15.
+def somatorio(n):
+    soma = 0
+    for i in range(1, n + 1):
+        soma += i
+    return soma
+
+
+print(somatorio(5))
+
+
+# Dica: a função sum já existe nativamente no Python!
+def somatorio(n):
+    return sum(range(1, n + 1))
+
+
+print(somatorio(5))
+
 
 # Exercício 4: Um posto está vendendo combustíveis com a seguinte tabela de
 # descontos:
