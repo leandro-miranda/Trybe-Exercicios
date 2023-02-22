b921f48b50370abfa68afc51c1bba64bf27f5e70
leandro-miranda <leandromiranda2010@gmail.com>
2023-02-22 17:45:20 -0300

    exercicio 1 feito

--- 4°-modulo-ciencia-da-computacao/dia-03-testes/exercicios/exercicio1.py
+++ 4°-modulo-ciencia-da-computacao/dia-03-testes/exercicios/exercicio1.py
@@ -8,4 +8,18 @@
 # Números divisíveis por 3 e 5 devem aparecer como “FizzBuzz” ao invés do
 # número.
 
+
 # Exemplo: 3 -> [1, 2, "Fizz"].
+# if number % 15 == 0:
+def fizzbuzz(n):
+    numbers = []
+    for number in range(1, n + 1):
+        if number % 3 == 0 and number % 5 == 0:
+            numbers.append("FizzBuzz")
+        elif number % 3 == 0:
+            numbers.append("Fizz")
+        elif number % 5 == 0:
+            numbers.append("Buzz")
+        else:
+            numbers.append(number)
+    return numbers
